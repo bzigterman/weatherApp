72221c4a54c059fdd03a0a6d170dd818ffd736fa
Ben Zigterman <ben@bzigterman.com>
2024-05-27 12:13:02 -0500

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Ben Zigterman on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
