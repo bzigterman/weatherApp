cca67c9db846fa8d0c095b264053e35178a5003f
Ben Zigterman <ben@bzigterman.com>
2024-05-30 16:19:07 -0500

    rearrange humdity and feels like

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -8,18 +8,18 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if let location = locationManager.location {
+               
+                if let weather = weather {
+                    Text("Temperature: \(String(format: "%.0f", weather.temp.rounded()))°F")
+                    Text("Feels Like: \(String(format: "%.0f", weather.feels_like.rounded()))°F")
+                    Text("Humidity: \(String(format: "%.0f", weather.humidity.rounded()))%")
+                }
                 Button(action: {
                     fetchWeather(for: location)
                 }) {
                     Text("Refresh Weather for Current Location")
                 }
                 .padding()
-
-                if let weather = weather {
-                    Text("Temperature: \(String(format: "%.0f", weather.temp.rounded()))°F")
-                    Text("Humidity: \(String(format: "%.0f", weather.humidity.rounded()))%")
-                    Text("Feels Like: \(String(format: "%.0f", weather.apparent_temperature.rounded()))°F")
-                }
             } else {
                 Text("Fetching location...")
             }
@@ -28,7 +28,7 @@ struct ContentView: View {
     }
 
     func fetchWeather(for location: CLLocationCoordinate2D) {
-        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=\(location.latitude)&longitude=\(location.longitude)&current=temperature_2m,relative_humidity_2m,apparent_temperature&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,uv_index&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&past_days=1"
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=\(location.latitude)&longitude=\(location.longitude)&current=temperature_2m,relative_humidity_2m,apparent_temperature&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,uv_index&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&past_days=0"
 
         guard let url = URL(string: urlString) else { return }
 
@@ -37,7 +37,8 @@ struct ContentView: View {
                 if let decodedResponse = try? JSONDecoder().decode(WeatherResponse.self, from: data) {
                     DispatchQueue.main.async {
                         self.weather = Weather(temp: decodedResponse.current.temperature_2m, humidity:decodedResponse.current.relative_humidity_2m,
-                                               apparent_temperature:decodedResponse.current.apparent_temperature)
+                                               feels_like:decodedResponse.current.apparent_temperature,uv: decodedResponse.hourly.uv_index[0],
+                                               temp_forecast:decodedResponse.hourly.temperature_2m[0])
                     }
                     return
                 }
@@ -49,7 +50,9 @@ struct ContentView: View {
 struct Weather {
     let temp: Double
     let humidity: Double
-    let apparent_temperature: Double
+    let feels_like: Double
+    let uv: Double
+    let temp_forecast: Double
 }
 
 struct WeatherResponse: Codable {
@@ -58,7 +61,12 @@ struct WeatherResponse: Codable {
         let relative_humidity_2m: Double
         let apparent_temperature: Double
     }
+    struct Hourly: Codable {
+        let temperature_2m: [Double]
+        let uv_index: [Double]
+    }
     let current: Current
+    let hourly: Hourly
 }
 
 struct ContentView_Previews: PreviewProvider {
